69f6892bbb47444dffc05ad12f83abce346c7692
Pragati Sahu <pragatisahu1234@gmail.com>
2023-07-12 16:55:30 +0530

    Day 1 to Day 4

---
+++ day_3_Python.py
@@ -0,0 +1,24 @@
+# print("I am learning Python")
+# name ="pragati"
+a=90
+print("Value of a ")
+print(a)
+print("value of name is "+ name)
+
+if 5>3:
+    print("Correct Indentation")
+
+
+print("\\")
+print("I\'m Pragati")
+print("This is my \n house")
+
+"""
+This is a multi-line comment
+I am learning Python
+This is my day 3
+and I am learning about multi-line comments
+"""
+
+myname = input("What is your name?")
+print(myname)
