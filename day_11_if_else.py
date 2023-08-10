b3e870a86512aabe32137927f2110cc8cb68ccb4
Pragati Sahu <pragatisahu1234@gmail.com>
2023-08-10 22:47:33 +0530

    Day 11 Done

--- day_11_if_else.py
+++ day_11_if_else.py
@@ -1,6 +1,21 @@
-# python program to illustrate If statement
-i = 10
-if (i > 15):
-	print("i is greater than 15")
-print("Condition not true ")
+# python program to illustrate If-elif-else ladder
+percent =76
+if (percent>=90 and percent<=100):
+	grade = 'A'
+elif (percent>=75 and percent<90):
+	grade ='B'
+elif (percent>=60 and percent<75):
+	grade = 'C'
+elif (percent>=50 and percent<60):
+	grade ='D'
+else:
+	grade ='F'
+print(grade)
+
+#Shorthand if statement
+i=9
+if(i>10) : print("i is greater")
+
+
+
 
