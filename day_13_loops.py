b2ec3ec27dceb1a29e48b168a0bd916d8608df55
Pragati Sahu <pragatisahu1234@gmail.com>
2023-09-15 21:27:51 +0530

    Loops Done

---
+++ day_13_loops.py
@@ -0,0 +1,59 @@
+# colors = ["red", "black", "yellow"]
+# for x in colors:
+# 	print(x)
+
+# for x in "Pragati":
+# 	print(x)
+
+# #Range Function
+# for x in range(11):
+# 	print(x)
+
+# for x in range(2, 7):
+# 	print(x)
+
+# for x in range(2, 30, 3):
+# 	print(x)
+
+# #Else in For Loop
+# for x in range(6):
+# 	print(x)
+# else:
+# 	print("Finally finished!")
+
+# #Break Statement
+# colors = ["red", "black", "yellow"]
+# for x in fruits:
+#     print(x)	
+#     if x == "banana":
+#         break
+
+# #Continue Statement 
+# colors = ["red", "black", "yellow"]
+# for x in fruits:
+#   if x == "banana":
+#     continue
+#   print(x)
+
+#   #while loop
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+  #Nested-Loops
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+	
+for x in adj:
+	for y in fruits:
+		print(x, y)
+
+
+
+
+
+
+
+
+
---
+++ python_turtle/microsoft_logo.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+colors =['#f65314','#7cbb00', '#ffbb00', '#00a1f1' ]
+pos = [(0,0), (100,0), (0,-100), (100,-100)]
+for (x,y), color in zip(pos,colors):
+    up()
+    goto(x,y)
+    down()
+    fillcolor(color)
+    begin_fill()
+    for i in range(4):
+        forward(90)
+        right(90)
+    end_fill()
