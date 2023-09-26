a6854d4e8b04e918310b30e3477a74a0fe21f8c2
Pragati Sahu <pragatisahu1234@gmail.com>
2023-09-26 20:10:38 +0530

    Some Changes

--- day_13_loops.py
+++ day_13_loops.py
@@ -1,45 +1,45 @@
-# colors = ["red", "black", "yellow"]
-# for x in colors:
-# 	print(x)
+colors = ["red", "black", "yellow"]
+for x in colors:
+	print(x)
 
-# for x in "Pragati":
-# 	print(x)
+for x in "Pragati":
+	print(x)
 
-# #Range Function
-# for x in range(11):
-# 	print(x)
+#Range Function
+for x in range(11):
+	print(x)
 
-# for x in range(2, 7):
-# 	print(x)
+for x in range(2, 7):
+	print(x)
 
-# for x in range(2, 30, 3):
-# 	print(x)
+for x in range(2, 30, 3):
+	print(x)
 
-# #Else in For Loop
-# for x in range(6):
-# 	print(x)
-# else:
-# 	print("Finally finished!")
+#Else in For Loop
+for x in range(6):
+	print(x)
+else:
+	print("Finally finished!")
 
-# #Break Statement
-# colors = ["red", "black", "yellow"]
-# for x in fruits:
-#     print(x)	
-#     if x == "banana":
-#         break
+#Break Statement
+colors = ["red", "black", "yellow"]
+for x in fruits:
+    print(x)	
+    if x == "banana":
+        break
 
-# #Continue Statement 
-# colors = ["red", "black", "yellow"]
-# for x in fruits:
-#   if x == "banana":
-#     continue
-#   print(x)
+#Continue Statement 
+colors = ["red", "black", "yellow"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)
 
-#   #while loop
-# i = 1
-# while i < 6:
-#   print(i)
-#   i += 1
+  #while loop
+i = 1
+while i < 6:
+  print(i)
+  i += 1
 
   #Nested-Loops
 adj = ["red", "big", "tasty"]
